fd032699fce5650e9c04ea00e9bb72fbf8b03da2
kunal59428 <kumarsharmakunal@gmail.com>
2025-01-04 11:47:45 +0530

    port commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 3000;
+
+const PORT = process.env.PORT
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -15,6 +16,6 @@ app.get("/login", (req, res) => {
   res.send("<h1>Neha hai</h1>");
 });
 
-app.listen(process.env.port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
